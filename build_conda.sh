cef5d749f51a79f58dc3c9bc457ca469c82c68c2
Mingxun Wang <mwang87@gmail.com>
2018-09-19 15:20:08 -0700

    adding build files for conda

--- build_conda.sh
+++ build_conda.sh
@@ -1,4 +1,9 @@
 conda-build . \
+ -c https://conda.anaconda.org/qiime2/label/r2018.4 \
+ -c https://conda.anaconda.org/qiime2 \
+ -c https://conda.anaconda.org/conda-forge \
+ -c defaults \
+ -c https://conda.anaconda.org/bioconda \
+ -c https://conda.anaconda.org/biocore \
  --override-channels \
  --python 3.5
- q2_metabolomics
